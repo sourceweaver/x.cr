5586d3c0234c6384ee343f2f7d4e36e90079b9d8
sourceweaver <sw.git@pm.me>
2022-11-13 20:51:30 +0300

    bump version, update entry point Signed-off-by: sourceweaver <sw.git@pm.me>

---
+++ src/x.cr
@@ -0,0 +1,13 @@
+#+------------------------------------------------------------------------+#
+#| Copyright © 2022 sourceweaver                                          |#
+#| Licensed under the Mozilla Public License, Version 2.0 (the "License").|#
+#| You may not use this file except in compliance with the License. You   |#
+#| may obtain a copy of the License at http://mozilla.org/MPL/2.0/        |#
+#+------------------------------------------------------------------------+#
+require "./debug"
+require "./assert"
+require "./unit_test"
+
+module X
+  VERSION = "0.1.1"
+end
